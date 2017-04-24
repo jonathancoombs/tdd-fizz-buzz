16450b96990caffb50e4d0e9c473661266663634
Jonathan Coombs <jonathan.coombs@discoverorg.com>
2017-04-24 14:02:43 -0700

    Refactor to use constants

--- src/main/java/com/discoverorg/service/FizzBuzz.java
+++ src/main/java/com/discoverorg/service/FizzBuzz.java
@@ -1,21 +1,20 @@
 package com.discoverorg.service;
 
 class FizzBuzz {
-//    private static int FIZZ_VALUE = 3;
-//    private static int BUZZ_VALUE = 5;
-//    private static String FIZZ_OUTPUT = "Fizz";
-//    private static String BUZZ_OUTPUT = "Buzz";
+    static final int FIZZ_VALUE = 3;
+    static final int BUZZ_VALUE = 5;
+    static final String FIZZ_OUTPUT = "Fizz";
+    static final String BUZZ_OUTPUT = "Buzz";
 
     static String playFizzBuzz(Integer input) {
         String output = input.toString();
-        if (input % 3 == 0 && input % 5 == 0) {
-            output = "FizzBuzz";
-        } else if (input % 3 == 0) {
-            output = "Fizz";
-        } else if (input % 5 == 0) {
-            output = "Buzz";
+        if (input % FIZZ_VALUE == 0 && input % BUZZ_VALUE == 0) {
+            output = FIZZ_OUTPUT+BUZZ_OUTPUT;
+        } else if (input % FIZZ_VALUE == 0) {
+            output = FIZZ_OUTPUT;
+        } else if (input % BUZZ_VALUE == 0) {
+            output = BUZZ_OUTPUT;
         }
         return output;
-//        return null;
     }
 }
