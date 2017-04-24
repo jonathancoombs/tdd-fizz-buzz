c1f5942b1e8c83a9baef59a33cbdf030bb7bf80b
Jonathan Coombs <jonathan.coombs@discoverorg.com>
2017-04-24 13:47:01 -0700

    Successful first test

--- src/main/java/com/discoverorg/service/FizzBuzz.java
+++ src/main/java/com/discoverorg/service/FizzBuzz.java
@@ -1,7 +1,21 @@
 package com.discoverorg.service;
 
 class FizzBuzz {
+//    private static int FIZZ_VALUE = 3;
+//    private static int BUZZ_VALUE = 5;
+//    private static String FIZZ_OUTPUT = "Fizz";
+//    private static String BUZZ_OUTPUT = "Buzz";
+
     static String playFizzBuzz(Integer input) {
-        return null;
+        String output = input.toString();
+//        if (input % FIZZ_VALUE == 0 && input % BUZZ_VALUE == 0) {
+//            output = FIZZ_OUTPUT+BUZZ_OUTPUT;
+//        } else if (input % FIZZ_VALUE == 0) {
+//            output = FIZZ_OUTPUT;
+//        } else if (input % BUZZ_VALUE == 0) {
+//            output = BUZZ_OUTPUT;
+//        }
+        return output;
+//        return null;
     }
 }
